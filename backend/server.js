c847b97cc77eafd1b36a45f15dba3bf88c267da2
LavanyaSingla <lavanyasingla322@gmail.com>
2024-05-09 20:10:57 +0530

    Handled Uncaugh Exception

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,19 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
 
-
 // Connect config
 dotenv.config({ path: "backend/config/config.env" });
 const PORT = process.env.PORT;
 
+// Handled Uncaught Exception
+process.on("uncaughtException",(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to unhandled promise rejection`);
+    process.exit(1);
+});
+console.log(jospw);
 //connect db
 // connectDB()
-
 
 const server= app.listen(PORT, () => {
     console.log(`Server is running on port :${PORT}`);
